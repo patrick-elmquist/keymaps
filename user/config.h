422aab8a7e49a01c1b53338ac9aaca5a3b76b3d9
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2021-11-27 20:59:10 +0100

    Removed duplication in config.h

--- user/config.h
+++ user/config.h
@@ -9,5 +9,16 @@
 #define COMBO_TERM_PER_COMBO
 #define COMBO_MUST_TAP_PER_COMBO
 
+#define ONESHOT_TAP_TOGGLE 2
+#define ONESHOT_TIMEOUT 3000
+
+#define TAPPING_TERM_PER_KEY
+#define TAPPING_FORCE_HOLD_PER_KEY
+
+#define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
+#define PERMISSIVE_HOLD_PER_KEY
+
+#define SPLIT_MODS_ENABLE
+
 // Should reduce the total firmware footprint according to some blog post
 #define DISABLE_LEADER
